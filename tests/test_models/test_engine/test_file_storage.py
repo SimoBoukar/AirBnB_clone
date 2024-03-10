28eadb029d8a378fd52f57963dcd27ef57e0e44e
Rafiki_Mostafa <rafikimostafa@gmail.com>
2024-03-10 15:57:50 +0000

    add Unittest for file_storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+    Tests for FileStorage
+"""
+import unittest
+import json
+from models.engine.file_storage import FileStorage, BaseModel, User, State
+from models.engine.file_storage import City, Amenity, Place, Review
+from models.base_model import BaseModel
+
+
+class TestFileStorage(unittest.TestCase):
+    """Tests cases for FileStorage"""
+
+    def setUp(self):
+        """Prepare the test fixture before each test method."""
+        self.storage = FileStorage()
+
+    def test_all_returns_dict(self):
+        """Test if 'all' method returns a dictionary."""
+        self.assertIsInstance(self.storage.all(), dict)
+
+    def test_new_method(self):
+        """Test the 'new' method for adding objects."""
+        obj = BaseModel()
+        self.storage.new(obj)
+        self.assertIn(obj, self.storage.all().values())
+
+    def test_save_method(self):
+        """Test the 'save' method."""
+        obj = BaseModel()
+        self.storage.new(obj)
+        obj_id = obj.id
+        k = "{}.{}".format(obj.__class__.__name__, obj_id)
+        self.storage.save()
+        with open(FileStorage._FileStorage__file_path, 'r') as f:
+            data = json.load(f)
+        self.assertIn(k, data.keys())
+
+    def test_reload_method(self):
+        """Test the 'reload' method."""
+        self.storage.reload()
+        self.assertTrue(hasattr(self.storage, '_FileStorage__objects'))
+        self.assertIsInstance(self.storage._FileStorage__objects, dict)
+
+
+if __name__ == '__main__':
+    unittest.main()
